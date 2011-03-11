32bb6915a6820640590e4eeb0954d5b5e5aa4c6c
aldeirm2 <aldeirm2@gmail.com>
2011-03-11 13:04:59 +0000

    new

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :authorizations
-  has_many :critical_processes, :through => :authorizations, :primary_key => :cp_secondary_id
+  has_many :critical_processes, :through => :authorizations
   has_many :assignments
   has_many :users, :through => :assignments
 
